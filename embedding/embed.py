9d550735657499e1b3c8fe582007150c036500c7
Derick Gross <derick.gross@gmail.com>
2023-11-27 01:39:30 -0500

    remove LSU data gathering

--- embedding/embed.py
+++ embedding/embed.py
@@ -63,26 +63,6 @@ def remove_newlines(series):
   series = series.str.replace('  ', ' ')
   return series
 
-
-# Get all the text files in the text directory
-# for file in os.listdir("text/" + DOMAIN + "/"):
-# for file in os.listdir(DOMAIN + "/"):
-
-#   # Open the file and read the text
-#   with open("text/" + DOMAIN + "/" + file, "r", encoding="UTF-8") as f:
-#     text = f.read()
-#     # we replace the last 4 characters to get rid of .txt, and replace _ with / to generate the URLs we scraped
-#     filename = file[:-4].replace('_', '/')
-#     """
-#     There are a lot of contributor.txt files that got included in the scrape, this weeds them out. There are also a lot of auth required urls that have been scraped to weed out as well
-#     """ 
-#     if filename.endswith(".txt") or 'users/fxa/login' in filename:
-#       continue
-
-#     # then we replace underscores with / to get the actual links so we can cite contributions
-#     texts.append(
-#       (filename, text))
-
 # Create a dataframe from the list of texts
 df = pd.DataFrame(texts, columns=['text'])
 
