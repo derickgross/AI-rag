afe6c55bfc6ffe4ac7f40a84a161fa38e45b228b
Derick Gross <derick.gross@gmail.com>
2024-02-04 14:25:28 -0500

    restore noah hein embedding default

--- embedding/embed.py
+++ embedding/embed.py
@@ -1,60 +1,16 @@
+# https://github.com/nheingit/AI-101/blob/step-1/embedding/embed.py
 import pandas as pd
 import os
 import tiktoken
 import openai
-import urllib.request
-import json
 from dotenv import load_dotenv
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 load_dotenv()
 openai.api_key = os.environ['OPENAI_API_KEY']
 
-urlData = "https://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard"
+DOMAIN = "developer.mozilla.org"
 
-request = urllib.request.Request(
-    urlData,
-    headers={
-        "User-Agent": "Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11"
-    }
-)
-webURL = urllib.request.urlopen(request)
-raw_data = webURL.read()
-data = json.loads(raw_data.decode())
-
-  # Create a list to store the text files
-texts=[]
-
-season = data["events"][0]["season"]["year"]
-week = data["events"][0]["week"]["number"]
-
-for game in data["events"]:
-  summary = ""
-
-  teams = game["name"].split(" at ")
-  road_team = teams[0]
-  home_team = teams[1]
-  home_score = game["competitions"][0]["competitors"][0]["score"]
-  road_score = game["competitions"][0]["competitors"][1]["score"]
-
-  summary += f"In week {week} of the {season} NFL season, the {home_team} hosted the {road_team}."
-  if "winner" in game["competitions"][0]["competitors"][0].keys():
-    if game["competitions"][0]["competitors"][0]["winner"]:
-        summary += f" The {home_team} won the game, by a score of {home_score}-{road_score}."
-    else:
-        summary += f" The {road_team} won the game, by a score of {road_score}-{home_score}."
-  else:
-    summary += " The game is still in progress."
-    if int(home_score) > int(road_score):
-      summary += f" The {home_team} is leading by a score of {home_score}-{road_score}."
-    if int(road_score) > int(home_score):
-      summary += f" The {road_team} is leading by a score of {road_score}-{home_score}."
-    if int(road_score) == int(home_score):
-      summary += f" The score is tied at {home_score}."
-#   for headline in game["headlines"]:
-
-
-  texts.append(summary)
 
 def remove_newlines(series):
   series = series.str.replace('\n', ' ')
@@ -63,19 +19,41 @@ def remove_newlines(series):
   series = series.str.replace('  ', ' ')
   return series
 
+
+  # Create a list to store the text files
+texts=[]
+
+# Get all the text files in the text directory
+for file in os.listdir("text/" + DOMAIN + "/"):
+
+  # Open the file and read the text
+  with open("text/" + DOMAIN + "/" + file, "r", encoding="UTF-8") as f:
+    text = f.read()
+    # we replace the last 4 characters to get rid of .txt, and replace _ with / to generate the URLs we scraped
+    filename = file[:-4].replace('_', '/')
+    """
+    There are a lot of contributor.txt files that got included in the scrape, this weeds them out. There are also a lot of auth required urls that have been scraped to weed out as well
+    """ 
+    if filename.endswith(".txt") or 'users/fxa/login' in filename:
+      continue
+
+    # then we replace underscores with / to get the actual links so we can cite contributions
+    texts.append(
+      (filename, text))
+
 # Create a dataframe from the list of texts
-df = pd.DataFrame(texts, columns=['text'])
+df = pd.DataFrame(texts, columns=['fname', 'text'])
 
 
 # Set the text column to be the raw text with the newlines removed
-df['text'] = remove_newlines(df.text)
+df['text'] = df.fname + ". " + remove_newlines(df.text)
 df.to_csv('processed/scraped.csv')
 
 # Load the cl100k_base tokenizer which is designed to work with the ada-002 model
 tokenizer = tiktoken.get_encoding("cl100k_base")
 
 df = pd.read_csv('processed/scraped.csv', index_col=0)
-df.columns = ['text']
+df.columns = ['title', 'text']
 
 # Tokenize the text and save the number of tokens to a new column
 df['n_tokens'] = df.text.apply(lambda x: len(tokenizer.encode(x)))
