c98a3c6fdee964e25b4453034818702abd4d7cb4
Derick Gross <derick.gross@gmail.com>
2024-02-27 16:28:23 -0500

    rename handler

---
+++ cdk/ai-rag/lambdas/lambda_answer_question_stack.py
@@ -0,0 +1,19 @@
+from aws_cdk import aws_apigateway as apigw
+
+class LambdaAnswerQuestionStack(Stack):
+    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+
+        # create lambda function
+        question_handler = _lambda.Function(
+            self, "handle_question_function",
+            runtime=_lambda.Runtime.PYTHON_3_7,
+            handler="question-handler.handle_question",
+            code=_lambda.Code.from_asset("./lambda")
+        )
+
+        # create API Gateway endpoint that handles questions
+        apigw.LambdaRestApi(
+            self, 'Endpoint',
+            handler=question_handler,
+        )
