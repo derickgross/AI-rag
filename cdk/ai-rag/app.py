57c126d5c606de9de090a9c354ad3f996b510a24
Derick Gross <derick.gross@gmail.com>
2024-02-27 15:54:28 -0500

    add embeddings and answer question stacks

--- cdk/ai-rag/app.py
+++ cdk/ai-rag/app.py
@@ -3,8 +3,13 @@
 from aws_cdk import App
 
 from s3trigger.s3trigger_stack import S3TriggerStack
+from s3.s3bucket_embeddings_stack import S3BucketEmbeddingsStack
+from lambdas.lambda_answer_question_stack import LambdaAnswerQuestionStack
 
 app = App()
 S3TriggerStack(app, "s3trigger")
+S3BucketEmbeddingsStack(app, "s3embeddings")
+LambdaAnswerQuestionStack(app, "answerQuestion")
+
 
 app.synth()
