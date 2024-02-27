a9254a04bf3e1e5cac6d7e8430af7b4a9a444573
Derick Gross <derick.gross@gmail.com>
2024-02-27 12:58:14 -0500

    create embeddings bucket stack

---
+++ cdk/ai-rag/s3/s3bucket_embeddings_stack.py
@@ -0,0 +1,26 @@
+# export class S3BucketStack extends cdk.Stack {
+#   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+#     super(scope, id, props);
+
+#     const s3Bucket = new s3.Bucket(this, 'exampleBucket', {
+#       objectOwnership: s3.ObjectOwnership.BUCKET_OWNER_ENFORCED,
+#       blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+#       encryptionKey: new kms.Key(this, 's3BucketKMSKey'),
+#     });
+
+#     s3Bucket.grantRead(new iam.AccountRootPrincipal());
+#   }
+# }
+
+from aws_cdk import(
+    aws_s3 as _s3,
+    Stack
+)
+from constructs import Construct
+
+class S3BucketEmbeddingsStack(Stack):
+    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+
+    # create s3 bucket
+    s3 = _s3.Bucket(self, "embeddingsBucket")
