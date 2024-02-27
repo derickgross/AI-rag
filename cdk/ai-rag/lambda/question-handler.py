b5532735db004de9a867fe6f7bab4432f2ff5bac
Derick Gross <derick.gross@gmail.com>
2024-02-27 15:36:04 -0500

    answer question in handler

--- cdk/ai-rag/lambda/question-handler.py
+++ cdk/ai-rag/lambda/question-handler.py
@@ -1,8 +1,14 @@
+from questions import answer_question
+
 def handle_question(event, context):
     # save event to logs
     print(event)
 
+    question = event['body']['question']
+
+    answer = answer_question(question=question)
+
     return {
         'statusCode': 200,
-        'body': event
+        'body': answer
     }
