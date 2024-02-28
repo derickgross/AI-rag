fe2462e5ce84a469a1e41b0aadf8116229c12363
Derick Gross <derick.gross@gmail.com>
2024-02-28 10:04:13 -0500

    test module

--- cdk/ai-rag/app.py
+++ cdk/ai-rag/app.py
@@ -1,16 +1,11 @@
-#!/usr/bin/env python3
+from aws_cdk import App
 
-# import sys
-# sys.path.insert(0, '/home/runner/work/AI-rag')
-
-# from aws_cdk import App
-
-# from s3trigger.s3trigger_stack import S3TriggerStack
-# from s3.s3bucket_embeddings_stack import S3BucketEmbeddingsStack
+from s3trigger.s3trigger_stack import S3TriggerStack
+from s3.s3bucket_embeddings_stack import S3BucketEmbeddingsStack
 from lambdas.lambda_answer_question_stack import LambdaAnswerQuestionStack
 
 app = App()
-# S3TriggerStack(app, "s3trigger")
+S3TriggerStack(app, "s3trigger")
 S3BucketEmbeddingsStack(app, "s3embeddings")
 LambdaAnswerQuestionStack(app, "answerQuestion")
 
