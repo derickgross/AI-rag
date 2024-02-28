39588000fe07720dcfe2733ca0de04abc5ad32cc
Derick Gross <derick.gross@gmail.com>
2024-02-28 16:19:51 -0500

    update paths

--- cdk/ai-rag/app.py
+++ cdk/ai-rag/app.py
@@ -1,8 +1,8 @@
 from aws_cdk import App
 
-from ai_rag.s3trigger.s3trigger_stack import S3TriggerStack
-from ai_rag.s3.s3bucket_embeddings_stack import S3BucketEmbeddingsStack
-from ai_rag.lambdas.lambda_answer_question_stack import LambdaAnswerQuestionStack
+from s3trigger_stack import S3TriggerStack
+from s3bucket_embeddings_stack import S3BucketEmbeddingsStack
+from lambda_answer_question_stack import LambdaAnswerQuestionStack
 
 app = App()
 S3TriggerStack(app, "s3trigger")
