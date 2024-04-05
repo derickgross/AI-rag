bed58bad3f19cd387e4c80fa091f4acd43852122
Derick Gross <derick.gross@gmail.com>
2024-04-05 12:31:05 -0400

    add extension to path

--- cdk/ai-rag/lambdas/lambda_answer_question_stack.py
+++ cdk/ai-rag/lambdas/lambda_answer_question_stack.py
@@ -4,18 +4,20 @@ from aws_cdk import(
     aws_lambda as _lambda,
 )
 from constructs import Construct
-from os import path
+import os
 
 class LambdaAnswerQuestionStack(Stack):
     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
+
+        cwd = os.getcwd()
 
         # create lambda function
         question_handler = _lambda.Function(
             self, "handle_question_function",
             runtime=_lambda.Runtime.PYTHON_3_7,
             handler="question-handler.handle_question",
-            code=_lambda.Code.from_asset(path.join(path.dirname('./'), 'question-handler.py'))
+            code=_lambda.Code.from_asset(os.path.join(cwd, 'question-handler.py'))
         )
 
         # create API Gateway endpoint that handles questions
