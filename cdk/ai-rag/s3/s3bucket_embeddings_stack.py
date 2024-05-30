d18afa91835879c68b18a36c97b844d07773acc0
Derick Gross <derick.gross@gmail.com>
2024-05-30 16:51:48 -0400

    embed v2

--- cdk/ai-rag/s3/s3bucket_embeddings_stack.py
+++ cdk/ai-rag/s3/s3bucket_embeddings_stack.py
@@ -34,7 +34,7 @@ class S3BucketEmbeddingsStack(Stack):
 
         # create embed docs lambda function
         embed_docs_handler = _lambda.DockerImageFunction(
-            self, "handle_embed_docs_function",
+            self, "handle_embed_docs_function_v2",
             architecture=_lambda.Architecture.ARM_64,
             code=_lambda.DockerImageCode.from_ecr(repository=ecr_repository, tag="latest"),
         )
